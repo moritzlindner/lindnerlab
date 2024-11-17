f53960bf471738804ac52b4a3c462fa864a6c204
MoritzLindner <moritz.lindner@uni-marburg.de>
2024-11-17 05:31:22 +0100

    Luminance to photon flux fx added

--- R/lightunitconverter.R
+++ R/lightunitconverter.R
@@ -4,8 +4,7 @@ library(constants)
 
 #' @importFrom constants syms
 #' @importFrom units set_units
-# Load the units package
-convert_to_photons <- function(power_density, wavelength) {
+power_density_to_photon_flux <- function(power_density, wavelength) {
   # Check if inputs have compatible units
   if (!inherits(try(set_units(power_density, "mW/mm^2"), silent = TRUE), "units")) {
     stop("Input 'power_density' must be convertible to 'mW/mm^2'")
@@ -31,8 +30,27 @@ convert_to_photons <- function(power_density, wavelength) {
   return(photon_flux)
 }
 
-# Example usage:
+#' @importFrom units set_units
+ganzfeld_luminance_to_pupil_photon_flux <- function(scot_luminance) {
+  # https://www.sciencedirect.com/science/article/pii/S0042698904004468?via%3Dihub#sec1
+  if (!inherits(try(set_units(scot_luminance, "cd/m^2"), silent = TRUE)
+                , "units")) {
+    stop("Input 'scot_luminance' must be convertible to 'cd/m^2'")
+  }
+
+  fact <-
+    set_units(1.5 * 10 ^ 15, "1/(s*lm)")# the factor 1.5 × 1015 converts scotopic lumens to photon flux, and has the units (photons s−1 lumen−1).
+
+  pi*fact*scot_luminance
+}
+
+
+#' # Example usage:
 power_density <- set_units(0.1, "mW/mm^2")  # Power density in mW/mm^2
 wavelength <- set_units(610, "nm")        # Wavelength in nm
-
 convert_to_photons(power_density, wavelength)
+
+flash_energy=set_units(3, "cd*s/m^2")
+scot_luminance<-flash_energy/set_units(0.04,"s")#set_units(30, "cd/m^2")
+out<-ganzfeld_luminance_to_pupil_photon_flux(scot_luminance)
+format(set_units(out, "1/cm^2/s"), scientific = TRUE)
